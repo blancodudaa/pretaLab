bcf4f66af14f66fea276616984553ca7fecce2eb
blancodudaa <dev.dudablanco@outlook.com>
2023-02-15 11:07:32 -0300

    Exercícios do curso PretaLab

---
+++ pratica01.js
@@ -0,0 +1,5 @@
+const nome='Eduarda',
+    idade = 23,
+    signo = 'libra';
+
+console.log(nome, idade, signo)
---
+++ pratica02.js
@@ -0,0 +1,7 @@
+let nomePais= 'Brasil',
+    pontosTuristicos = 'Cristo Redentor',
+    capitalPais = 'Brasília',
+    qtdEstados = 26,
+    paisDaAmeriaLatina = true;
+
+console.log(nomePais,pontosTuristicos,capitalPais, qtdEstados, paisDaAmeriaLatina)
